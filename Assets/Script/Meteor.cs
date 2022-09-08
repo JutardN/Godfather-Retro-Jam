78cec9e24ef820a37ca63ffc39b6ddb36cb546e1
Nolan <nolan.jutard@gmail.com>
2022-09-08 17:59:13 +0200

    Add Ending Scene + Modif script

--- Assets/Script/Meteor.cs
+++ Assets/Script/Meteor.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Meteor : MonoBehaviour
 {
@@ -165,7 +166,7 @@ public class Meteor : MonoBehaviour
 
         if (transform.position.y == direction.y)
         {
-            Destroy(gameObject);
+            SceneManager.LoadScene(2);
         }
     }
    
